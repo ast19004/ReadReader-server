e9e17a2fc967d8c2c92b5c33d44545e1c1a6fa01
Anna Astle <ast19004@byui.edu>
2023-05-23 12:25:03 -0700

    multer upload to googleCloud

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,13 +1,12 @@
 const multer = require("multer");
+const { Storage } = require("@google-cloud/storage");
 
-const multerConfig = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "public/images/uploads");
-  },
-  filename: (req, file, callback) => {
-    callback(null, `${Date.now()}_${file.originalname}`);
-  },
+const googleStorage = new Storage({
+  keyFilename: "../readreader-aebdef56b5c6.json",
+  projectId: "readreader",
 });
+
+const bucket = googleStorage.bucket("prize-images");
 
 const isImage = (req, file, callback) => {
   if (file.mimetype.startsWith("image")) {
@@ -18,14 +17,71 @@ const isImage = (req, file, callback) => {
 };
 
 const upload = multer({
-  storage: multerConfig,
-  fileFilter: isImage,
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB file size limit (adjust as needed)
+  },
 });
 
 exports.uploadImage = upload.single("prize_image");
 
 exports.upload = (req, res, next) => {
+  if (!req.file) {
+    res.status(400).json({ error: "No file uploaded." });
+    return;
+  }
+
+  const filename = `${Date.now()}_${req.file.originalname}`;
+  const file = bucket.file(filename);
+
+  const stream = file.createWriteStream({
+    metadata: {
+      contentType: req.file.mimetype,
+    },
+  });
+
+  stream.on("error", (err) => {
+    req.file.cloudStorageError = err;
+    next(err);
+  });
+
+  stream.on("finish", () => {
+    req.file.cloudStorageObject = filename;
+    req.file.cloudStoragePublicUrl = getPublicUrl(filename);
+    next();
+  });
+
+  stream.end(req.file.buffer);
+
   res.status(200).json({
-    filename: req.file ? req.file.filename : "",
+    filename: filename,
+    publicUrl: req.file ? getPublicUrl(filename) : "",
   });
 };
+
+// Helper function to get public URL of the uploaded file
+function getPublicUrl(filename) {
+  return `https://storage.googleapis.com/prize-images/${filename}`;
+}
+
+// const multerConfig = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, "public/images/uploads");
+//   },
+//   filename: (req, file, callback) => {
+//     callback(null, `${Date.now()}_${file.originalname}`);
+//   },
+// });
+
+// const isImage = (req, file, callback) => {
+//   if (file.mimetype.startsWith("image")) {
+//     callback(null, true);
+//   } else {
+//     callback(new Error("Only Images are Allowed..."));
+//   }
+// };
+
+// const upload = multer({
+//   storage: multerConfig,
+//   fileFilter: isImage,
+// });
