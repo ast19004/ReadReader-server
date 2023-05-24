506e14dfba971f5a6d621567de02e7534f33270a
Anna Astle <ast19004@byui.edu>
2023-05-23 17:00:30 -0700

    upload testing

--- middleware/upload.js
+++ middleware/upload.js
@@ -6,7 +6,9 @@ const googleStorage = new Storage({
   projectId: "readreader",
 });
 
-const bucket = googleStorage.bucket("prize-images");
+const bucketName = "prize-images";
+
+const bucket = googleStorage.bucket(bucketName);
 
 const isImage = (req, file, callback) => {
   if (file.mimetype.startsWith("image")) {
@@ -21,6 +23,7 @@ const upload = multer({
   limits: {
     fileSize: 5 * 1024 * 1024, // 5MB file size limit (adjust as needed)
   },
+  fileFilter: isImage,
 });
 
 exports.uploadImage = upload.single("prize_image");
@@ -30,6 +33,7 @@ exports.upload = (req, res, next) => {
     res.status(400).json({ error: "No file uploaded." });
     return;
   }
+  console.log(`file: ${JSON.stringify(req.file)}`);
 
   const filename = `${Date.now()}_${req.file.originalname}`;
   const file = bucket.file(filename);
@@ -52,7 +56,7 @@ exports.upload = (req, res, next) => {
   });
 
   stream.end(req.file.buffer);
-
+  console.log(`publicUrl: ${getPublicUrl(filename)}`);
   res.status(200).json({
     filename: filename,
     publicUrl: req.file ? getPublicUrl(filename) : "",
@@ -61,7 +65,7 @@ exports.upload = (req, res, next) => {
 
 // Helper function to get public URL of the uploaded file
 function getPublicUrl(filename) {
-  return `https://storage.googleapis.com/prize-images/${filename}`;
+  return `https://storage.googleapis.com/${bucket.name}/${filename}`;
 }
 
 // const multerConfig = multer.diskStorage({
