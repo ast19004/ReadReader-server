a624f7a7ad472fe30e81ee6d2686da6400db1180
Anna Astle <ast19004@byui.edu>
2023-05-31 10:07:24 -0700

    build update

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,7 +3,7 @@ const Multer = require("multer");
 const { Storage } = require("@google-cloud/storage");
 
 const projectId = "readreader";
-const keyFilename = `/Users/alouisa/Desktop/ReadReader-server/${process.env.GOOGLE_APPLICATION_CREDENTIALS}`;
+const keyFilename = `/Users/alouisa/Desktop/ReadReader-server/google-credentials.json`;
 
 const googleStorage = new Storage({
   projectId,
@@ -14,20 +14,20 @@ const bucketName = "prize-images";
 
 const bucket = googleStorage.bucket(bucketName);
 
-// const isImage = (req, file, callback) => {
-//   if (file.mimetype.startsWith("image")) {
-//     callback(null, true);
-//   } else {
-//     callback(new Error("Only Images are Allowed..."));
-//   }
-// };
+const isImage = (req, file, callback) => {
+  if (file.mimetype.startsWith("image")) {
+    callback(null, true);
+  } else {
+    callback(new Error("Only Images are Allowed..."));
+  }
+};
 
 const multer = Multer({
   storage: Multer.memoryStorage(),
   limits: {
     fileSize: 5 * 1024 * 1024, // no larger than 5MB
   },
-  // fileFilter: isImage,
+  fileFilter: isImage,
 });
 
 exports.uploadImage = multer.single("prize_image");
@@ -57,30 +57,3 @@ exports.upload = (req, res, next) => {
     res.status(500).send(error);
   }
 };
-
-// Helper function to get public URL of the uploaded file
-function getPublicUrl(filename) {
-  return `https://storage.googleapis.com/${bucket.name}/${filename}`;
-}
-
-// const multerConfig = multer.diskStorage({
-//   destination: (req, file, callback) => {
-//     callback(null, "public/images/uploads");
-//   },
-//   filename: (req, file, callback) => {
-//     callback(null, `${Date.now()}_${file.originalname}`);
-//   },
-// });
-
-// const isImage = (req, file, callback) => {
-//   if (file.mimetype.startsWith("image")) {
-//     callback(null, true);
-//   } else {
-//     callback(new Error("Only Images are Allowed..."));
-//   }
-// };
-
-// const upload = multer({
-//   storage: multerConfig,
-//   fileFilter: isImage,
-// });
