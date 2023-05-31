a895c8612950aff99349124e9edb7d732531e5ac
Anna Astle <ast19004@byui.edu>
2023-05-30 21:46:21 -0700

    catch error upload console log

--- middleware/upload.js
+++ middleware/upload.js
@@ -4,7 +4,7 @@ const { Storage } = require("@google-cloud/storage");
 
 const projectId = "readreader";
 const keyFilename = path.join(
-  `${__dirname}${process.env.GOOGLE_APPLICATION_CREDENTIALS}`
+  `../${process.env.GOOGLE_APPLICATION_CREDENTIALS}`
 );
 
 const googleStorage = new Storage({
